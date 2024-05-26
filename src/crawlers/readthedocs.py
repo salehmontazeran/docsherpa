e4e29ae7880f982c50f0cb08f92fb317d63fc6ee
Saleh Montazeran <skytask61@gmail.com>
2024-05-26 08:14:39 +0330

    #4 - just init

---
+++ src/crawlers/readthedocs.py
@@ -0,0 +1,4 @@
+def readthedocs(project_slug: str) -> None:
+    base_url = "https://readthedocs.org/api/v3"
+    project_slug = "your-project-slug"  # Replace with your project's slug
+    version_slug = "latest"  # Replace with the desired version slug
